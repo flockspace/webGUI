3f966fa59d03900c9c59972f7df5ac64db8a505f
flockspace <flockspace2016@gmail.com>
2016-09-07 19:19:35 +0530

    modified configDB to check if space already exists or not. Fixed variable referencing bug

--- webApp/configDB.py
+++ webApp/configDB.py
@@ -6,13 +6,12 @@ Created on 25-Sep-2015
 import logging
 
 from cassandra.cluster import Cluster
-from backendServer import constants
 
 class configDB:
     cluster=None
     session=None
     log = None
-    keyspace = constants.CASSANDRA_KEYSPACE
+    keyspace = "configdbkeyspace"
     
     
     def __init__(self):
@@ -23,7 +22,8 @@ class configDB:
             logging.basicConfig(filename='/var/log/configDB.log', filemode='w', format='%(asctime)-15s:%(levelname)s:%(message)s', 
                             level=logging.DEBUG)
             self.Connect(["127.0.0.1"])
-            self.createNameSpace(self.keyspace)
+            if self.keyspace not in self.cluster.metadata.keyspaces.keys():
+	        self.createNameSpace(self.keyspace)
             logging.info("Cassandra DB is successfully initialized")
         except Exception as err:
                 raise Exception(err)
@@ -32,10 +32,10 @@ class configDB:
     """Connect to cassandra cluster"""
     def Connect(self, nodes):
         try:
-            cluster = Cluster(nodes)
-            metadata = cluster.metadata
+            self.cluster = Cluster(nodes)
+            metadata = self.cluster.metadata 
             self.session = None
-            self.session = cluster.connect()
+            self.session = self.cluster.connect()
             logging.info('Connected to cluster: ' + metadata.cluster_name)
             for host in metadata.all_hosts():
                 logging.info('Datacenter: %s; Host: %s; Rack: %s', host.datacenter, host.address, host.rack)
@@ -68,7 +68,7 @@ class configDB:
         except Exception as err:
             raise Exception(err)
         
-    def createTable(self, tableName=None, column_family=None, prime=None):
+    def createTable_(self, tableName=None, column_family=None, prime=None):
         if not (tableName and column_family and prime):
             return None
         try:
@@ -77,11 +77,29 @@ class configDB:
                 return self.Transaction(query);
         except Exception as err:
             raise Exception(err)
+    
+    def createTable(self,tableName=None,column_definition=dict(),primary_keys=[]):
+        if len(column_definition):
+            col_def=""
+            for key in column_definition.keys():
+                col_def+=""+key+" "+column_definition[key]+","
+        cql="CREATE TABLE "+self.keyspace+"."+tableName+" ( "+col_def
+        if primary_keys:
+            prim=""
+            for key in primary_keys[0:-1]:
+                prim+=" "+key+","
+            prim+=primary_keys[-1]
+            cql+=" PRIMARY KEY ("+prim+") );"
+        print cql
+        self.Transaction(cql);
 
 
 def main(): 
     db = configDB()
     db.initialization()
+    column_definition={"content_id":"uuid","userId":"uuid","location":"text"}
+    db.createTable("test_table", column_definition, ["content_id"])
+    
     
 if __name__ == '__main__':
     main()
